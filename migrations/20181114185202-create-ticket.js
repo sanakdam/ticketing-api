61cf5cbd7b072349fa66ec561722c3d50dc59915
akdam augusta <san.akdam@gmail.com>
2019-03-02 15:57:25 +0700

    add banyak data

--- migrations/20181114185202-create-ticket.js
+++ migrations/20181114185202-create-ticket.js
@@ -1,22 +1,16 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('tickets', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      code: {
-        type: Sequelize.STRING
-      },
-      status: {
-        type: Sequelize.TINYINT
-      }    
+    return queryInterface.createTable('ticket', {
+        code: DataTypes.STRING,
+        status: DataTypes.TINYINT,
+        outlet: DataTypes.STRING,
+        area: DataTypes.STRING,
+        strata: DataTypes.STRING,
+        information: DataTypes.STRING    
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('tickets');
+    return queryInterface.dropTable('ticket');
   }
 };
--- migrations/20181116151927-create-dashboard.js
+++ migrations/20181116151927-create-dashboard.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('dashboards', {
+    return queryInterface.createTable('dashboard', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -17,6 +17,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('dashboards');
+    return queryInterface.dropTable('dashboard');
   }
 };
