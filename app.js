16787335f14eb443c40b2296590a5bdce585b7b3
akdam augusta <san.akdam@gmail.com>
2019-03-02 15:32:21 +0700

    add banyak data

--- app.js
+++ app.js
@@ -1,56 +1,41 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const http = require('http');
+const app = express();
+const path = require('path');
 
-var index = require('./routes/index');
+global.app = express();
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use("/storages", express.static(path.join(process.cwd(), 'storages')));
 app.use(logger('dev'));
-app.use(bodyParser.json({limit: '50000kb'}));
-app.use(bodyParser.urlencoded({limit: '50000kb', extended: true}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json({limit: '500000kb'}));
+app.use(bodyParser.urlencoded({limit: '500000kb', extended: true}));
+
+var myLogger = function (req, res, next) {
+  console.log('LOGGED')
+  next()
+}
+
+app.use(myLogger)
+
+app.disable('x-powered-by');
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Origin");
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-
-  if (req.method === "OPTIONS") {
-      return res.status(200).end();
-  }
-  
-  return next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept-Language, Content-Language, Accept, Cache-Control, Authorization, Access-Control-Allow-Origin");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    next();
 });
 
 require('./routes/index')(app);
+app.get('*', (req, res) => res.status(404).send({
+    status: false,
+    message: 'No api route on this value.'
+}));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
+const port = parseInt(process.env.PORT, 10) || 8084;
+app.set('port', port);
+const server = http.createServer(app);
+server.listen(port);
 module.exports = app;
